90bae7be3ded7151458d6376d1430bfd7468b33c
Kulin <colgro@proton.me>
2023-04-03 18:37:19 +0200

    Delete examples

--- src/main/java/com/colgrolool/skidware/modules/ModuleExample.java
+++
@@ -1,9 +0,0 @@
-package com.colgrolool.skidware.modules;
-
-import meteordevelopment.meteorclient.systems.modules.Module;
-
-public class ModuleExample extends Module {
-    public ModuleExample() {
-        super(SkidModules.SKIDCAT, "example", "An example module in a custom category.");
-    }
-}
