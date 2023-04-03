9c30f0e2cb33988edb586b73bd34757425af4eb1
Kulin <colgro@proton.me>
2023-04-03 18:36:50 +0200

    Unused import

--- src/main/java/com/colgrolool/skidware/modules/ModuleExample.java
+++ src/main/java/com/colgrolool/skidware/modules/ModuleExample.java
@@ -1,6 +1,5 @@
 package com.colgrolool.skidware.modules;
 
-import com.colgrolool.skidware.SkidWare;
 import meteordevelopment.meteorclient.systems.modules.Module;
 
 public class ModuleExample extends Module {
